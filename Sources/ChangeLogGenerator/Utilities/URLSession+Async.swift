ffe07c2b4e19245b63121506e8093e62014f0bec
Mahmood Tahir <mahmood.tahir@thescore.com>
2021-12-29 09:37:28 -0500

    Expose method to linux

--- Sources/ChangeLogGenerator/Utilities/URLSession+Async.swift
+++ Sources/ChangeLogGenerator/Utilities/URLSession+Async.swift
@@ -15,26 +15,37 @@ enum URLSessionError: Error {
 }
 
 extension URLSession {
+    /// Perform a data task async which is available down to iOS 13 and macOS 10.15
+    /// - Parameter request: the request to perform
+    /// - Returns: a data and response object
     public func dataAsync(for request: URLRequest) async throws -> (Data, URLResponse) {
-        if #available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *) {
-            return try await data(for: request)
-        }
-        else {
-            return try await withUnsafeThrowingContinuation { continuation in
-                let task = dataTask(with: request) { data, response, error in
-                    if let data = data, let response = response {
-                        continuation.resume(returning: (data, response))
-                    }
-                    else if let error = error {
-                        continuation.resume(throwing: error)
-                    }
-                    else {
-                        continuation.resume(throwing: URLSessionError.emptyData)
-                    }
-                }
-
-                task.resume()
+        #if os(macOS) || os(iOS)
+            if #available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *) {
+                return try await data(for: request)
             }
+            else {
+                return try await makeAsync(for: request)
+            }
+        #else
+            return try await makeAsync(for: request)
+        #endif
+    }
+
+    private func makeAsync(for request: URLRequest) async throws -> (Data, URLResponse) {
+        try await withUnsafeThrowingContinuation { continuation in
+            let task = dataTask(with: request) { data, response, error in
+                if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                }
+                else if let error = error {
+                    continuation.resume(throwing: error)
+                }
+                else {
+                    continuation.resume(throwing: URLSessionError.emptyData)
+                }
+            }
+
+            task.resume()
         }
     }
 }
